73198817dbe2e7dd5203f80835adb38ad4d22cbc
Mariel de la Garza <mariel.code@gmail.com>
2020-03-09 13:40:01 -0400

    Create files, update title, spreadsheet key

---
+++ by-region/js/lines.js
@@ -0,0 +1,52 @@
+Highcharts.chart('hcContainer', {
+    // Load Data in from Google Sheets
+    data: {
+      googleSpreadsheetKey: '1oo2XGDoXRKy33Xz7I2urDekudeM-_zCK5xRttXEIymw',
+      googleSpreadsheetWorksheet: 1
+    },
+    // General Chart Options
+    chart: {
+      zoomType: 'x',
+      type: 'line'
+    },
+    // Chart Title and Subtitle
+    title: {
+      text: "All Countries"
+    },
+    subtitle: {
+      text: "Confirmed COVID-19 Cases by UN Region Through 3-8-20"
+    },
+    // Credits
+    credits: {
+      enabled: true,
+      href: false,
+      text: "CSIS Project Name | Source: NAME"
+    },
+    // Chart Legend
+    legend: {
+      title: {
+        text: 'Legend Title<br/><span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>'
+      },
+      align: 'center',
+      verticalAlign: 'bottom',
+      layout: 'horizontal'
+    },
+    // Y Axis
+    yAxis: {
+      title: {
+        text: "Number of Confirmed Cases"
+      }
+    },
+    // Additional Plot Options
+    plotOptions:
+    {
+      line: {
+        marker: {
+          enabled: false,
+          symbol: "circle",
+          radius: 3
+        },
+        lineWidth: 3
+      }
+    }
+  })
